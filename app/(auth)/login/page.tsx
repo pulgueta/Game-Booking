4b438e6503cf639f267d98e035dfaad2faee9e1a
pulgueta <retardix456@gmail.com>
2024-01-29 02:28:57 -0500

    Added auth pages

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+import { buttonVariants } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { LoginForm } from "@/components/form/login-form";
+
+const Login = () => {
+	return (
+		<section className='p-4 flex items-center justify-center'>
+			<Card className='w-full md:max-w-md'>
+				<CardHeader>
+					<CardTitle>Iniciar sesión</CardTitle>
+					<CardDescription>
+						Accede a tu cuenta para crear eventos
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<LoginForm />
+				</CardContent>
+				<CardFooter className='flex items-center justify-center'>
+					<Link
+						href='/register'
+						className={buttonVariants({ variant: "link" })}
+					>
+						¿No tienes cuenta? Haz clic aquí
+					</Link>
+				</CardFooter>
+			</Card>
+		</section>
+	);
+};
+export default Login;
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+import { RegisterForm } from "@/components/form/register-form";
+import { buttonVariants } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+
+const Register = () => {
+	return (
+		<section className='p-4 flex items-center justify-center'>
+			<Card className='w-full md:max-w-md'>
+				<CardHeader>
+					<CardTitle>Registro</CardTitle>
+					<CardDescription>
+						Crea tu cuenta para poder agendar eventos
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<RegisterForm />
+				</CardContent>
+				<CardFooter className='flex items-center justify-center'>
+					<Link
+						href='/login'
+						className={buttonVariants({ variant: "link" })}
+					>
+						¿Ya tienes cuenta? Inicia sesión
+					</Link>
+				</CardFooter>
+			</Card>
+		</section>
+	);
+};
+export default Register;
