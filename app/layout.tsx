33bac4856bfbbc40a138ab341a8a4b2ffd8a8b6a
pulgueta <retardix456@gmail.com>
2024-01-29 02:05:26 -0500

    Updated main layout to have all providers need

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-import { PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { SessionProvider } from "next-auth/react";
 
-import { Navbar } from "@/components/navbar";
+import { Navbar } from "@/components/navbar/navbar";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/providers/theme-switcher";
 import { auth } from "@/auth";
 
 import "./globals.css";
@@ -22,12 +24,22 @@ const RootLayout = async ({ children }: PropsWithChildren) => {
 	const session = await auth();
 
 	return (
-		<html lang='es'>
+		<html lang='es' suppressHydrationWarning>
 			<body className={inter.className}>
-				<SessionProvider session={session}>
-					<Navbar />
-					{children}
-				</SessionProvider>
+				<ThemeProvider
+					enableSystem
+					attribute='class'
+					defaultTheme='system'
+					storageKey='gb_theme'
+				>
+					<SessionProvider session={session}>
+						<Navbar />
+						<Toaster position='top-right' richColors />
+						<main className='flex min-h-[calc(100dvh-89px)] flex-col items-center justify-between p-4'>
+							{children}
+						</main>
+					</SessionProvider>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
