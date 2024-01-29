419c6751756bb9a7be5628569929acbd63a99635
pulgueta <retardix456@gmail.com>
2024-01-29 02:15:11 -0500

    Reusable submit button component for server actions-based forms

---
+++ components/form/submit-button.tsx
@@ -0,0 +1,20 @@
+import { useFormStatus } from "react-dom";
+
+import { Loader2Icon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+export const SubmitButton = ({ label }: { label: string }) => {
+	const { pending } = useFormStatus();
+
+	return (
+		<Button
+			type='submit'
+			className='w-full'
+			id='submit-btn'
+			disabled={pending}
+		>
+			{pending ? <Loader2Icon className='animate-spin' /> : label}
+		</Button>
+	);
+};
