4633b6c829eed6130dbb6d5693a71ddc8cabf1a9
pulgueta <retardix456@gmail.com>
2024-01-29 02:28:14 -0500

    Added custom fetch function used to fetch data from react server components

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,12 @@
+import { UrlObject } from "url";
+
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { env } from "@/envs.mjs";
+
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const rscFetch = async <T>(
+	endpoint: UrlObject | __next_route_internal_types__.RouteImpl<"">
+): Promise<T> => (await fetch(`${env.HOST}${endpoint}`)).json() as T;
