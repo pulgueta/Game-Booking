e20b377ee160ffcafc724f280e3f79b6bae1e227
pulgueta <retardix456@gmail.com>
2024-01-29 19:49:52 -0500

    Updated place fetching from RSC using custom fetcher

--- app/(booking)/page.tsx
+++ app/(booking)/page.tsx
@@ -1,15 +1,20 @@
 import { getPlaces } from "@/lib/data/get-data";
+import { rscFetch } from "@/lib/utils";
 import { CreateEventForm } from "./components/form/create-event-form";
+import { currentUser } from "@/lib/auth/current-user";
 
 const Booking = async () => {
-	const places = await getPlaces();
+	const userPromise = currentUser();
+	const placesPromise = rscFetch<typeof getPlaces>("/api/places");
+
+	const [user, places] = await Promise.all([userPromise, placesPromise]);
 
 	return (
 		<article className='border p-4 rounded w-full bg-secondary/50'>
 			<h3 className='text-2xl font-semibold tracking-tight mb-4'>
 				Crear nuevo evento
 			</h3>
-			<CreateEventForm places={places} />
+			<CreateEventForm places={places} email={user?.email as string} />
 		</article>
 	);
 };
