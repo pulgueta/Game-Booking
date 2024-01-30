087c4f78f7071107b254897407ae12242d1e167b
pulgueta <retardix456@gmail.com>
2024-01-30 05:06:28 -0500

    Imported repositories from dedicated files

--- lib/data/get-data.ts
+++ lib/data/get-data.ts
@@ -1,18 +1,12 @@
-import { Booking } from "@/models/booking.entity";
-import { AppDataSource } from "@/models/db";
-import { Place } from "@/models/place.entity";
+import { bookingRepository, placeRepository } from "@/models/db/repositories";
 
 export const getPlaces = async () => {
-	const placeRepository = (await AppDataSource).getRepository(Place);
-
 	const places = await placeRepository.find();
 
 	return places.map((places) => places);
 };
 
 export const getBookingsById = async (email: string) => {
-	const bookingRepository = (await AppDataSource).getRepository(Booking);
-
 	const bookings = await bookingRepository.findBy({
 		organizer: email,
 	});
@@ -21,8 +15,6 @@ export const getBookingsById = async (email: string) => {
 };
 
 export const getBookings = async () => {
-	const bookingRepository = (await AppDataSource).getRepository(Booking);
-
 	const bookings = await bookingRepository.find();
 
 	return bookings.map((booking) => booking);
