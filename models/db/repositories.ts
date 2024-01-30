547a758e8c79db35d86c6815185c72db74704517
pulgueta <retardix456@gmail.com>
2024-01-30 05:06:36 -0500

    Added user repository

--- models/db/repositories.ts
+++ models/db/repositories.ts
@@ -2,7 +2,9 @@ import { AppDataSource } from ".";
 import { Booking } from "../booking.entity";
 import { Comment } from "../comment.entity";
 import { Place } from "../place.entity";
+import { UserEntity } from "../user.entity";
 
 export const bookingRepository = (await AppDataSource).getRepository(Booking);
 export const placeRepository = (await AppDataSource).getRepository(Place);
 export const commentRepository = (await AppDataSource).getRepository(Comment);
+export const userRepository = (await AppDataSource).getRepository(UserEntity);
