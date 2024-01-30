c8293403efa5cf6823b8ebab1af5197312b78dd5
pulgueta <retardix456@gmail.com>
2024-01-29 19:50:14 -0500

    Update get user by email util

--- lib/auth/get-user-by-email.ts
+++ lib/auth/get-user-by-email.ts
@@ -1,12 +1,10 @@
 import { AppDataSource } from "@/models/db";
-import { User } from "@/models/user.entity";
+import { UserEntity } from "@/models/user.entity";
 
 export const getUserByEmail = async (email: string) => {
-	const userRepository = (await AppDataSource).getRepository(User);
-
-	const user = await userRepository.findOneBy({
-		email,
-	});
+	const user = await (
+		await AppDataSource
+	).manager.findOneBy(UserEntity, { email });
 
 	return user;
 };
