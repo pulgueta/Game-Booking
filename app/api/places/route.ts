b4e57b40912b7611322716cc942ec5619df510ef
pulgueta <retardix456@gmail.com>
2024-01-29 19:52:58 -0500

    Updated route handler in how fetches the places

--- app/api/places/route.ts
+++ app/api/places/route.ts
@@ -2,14 +2,8 @@ import { AppDataSource } from "@/models/db";
 import { Place } from "@/models/place.entity";
 
 export const GET = async () => {
-	const placeRepository = (await AppDataSource).getRepository(Place);
-
-	const places = (await placeRepository.find()).map(
-		({ availability, id, name }) => ({
-			availability,
-			id,
-			name,
-		})
+	const places = (await (await AppDataSource).manager.find(Place)).map(
+		(place) => place
 	);
 
 	return Response.json(places, { status: 200 });
