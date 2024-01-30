c7c9cdfc0809d8096adb103e1749e6ac11c8c52b
pulgueta <retardix456@gmail.com>
2024-01-29 19:16:33 -0500

    Added custom error pages

--- app/(booking)/@bookings/error.tsx
+++ app/(booking)/@bookings/error.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 const ErrorPage = ({ error }: { error: Error & { digest?: string } }) => {
 	return (
-		<aside className='border p-4 rounded w-full bg-secondary/50 md:col-span-2 lg:col-span-1 flex items-center justify-center flex-col gap-4'>
+		<aside className='border p-4 rounded w-full bg-secondary/50 md:col-span-2 lg:col-span-3 flex items-center justify-center flex-col gap-4 h-96'>
 			<h3 className='text-2xl font-semibold tracking-tight text-center'>
 				{error.message}
 			</h3>
