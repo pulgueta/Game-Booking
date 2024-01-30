408984b2996dc0d7bea7730e21a906a5380438a3
pulgueta <retardix456@gmail.com>
2024-01-29 19:20:17 -0500

    Made a custom fetch function for server components which call to the req.url of the host and appends the api endpoint needed based on the nextjs internal route types

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,6 +7,22 @@ import { env } from "@/envs.mjs";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
-export const rscFetch = async <T>(
-	endpoint: UrlObject | __next_route_internal_types__.RouteImpl<"">
-): Promise<T> => (await fetch(`${env.HOST}${endpoint}`)).json() as T;
+export type RSCFetch<T extends (...args: any) => any | any[]> = Awaited<
+	ReturnType<T>
+>;
+
+export type Method = "GET" | "POST";
+
+export const rscFetch = async <T extends (...args: any) => any | any[]>(
+	endpoint: UrlObject | __next_route_internal_types__.RouteImpl<"">,
+	body: BodyInit | null | undefined = null,
+	method: Method = "GET"
+): Promise<RSCFetch<T>> => {
+	const response = await fetch(`${env.HOST}${endpoint}`, { method, body });
+
+	if (!response.ok) {
+		throw new Error(`Failed to fetch data from ${endpoint}`);
+	}
+
+	return (await response.json()) as RSCFetch<T>;
+};
